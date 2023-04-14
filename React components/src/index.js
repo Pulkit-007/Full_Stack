65808c024f087d3bc5c9a9e6cc3f86cc024b4ebf
Pulkit Sharma <91002702+Pulkit-007@users.noreply.github.com>
2023-04-15 00:17:29 +0530

    React Components

---
+++ React components/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import Heading from "./Heading.jsx";
+// import List from "./List";
+import App from "./components/App";
+// ReactDOM.render(
+//   <div>
+//     <Heading />
+//     <List />
+//   </div>,
+//   document.getElementById("root")
+// );
+ReactDOM.render(<App />, document.getElementById("root"));
