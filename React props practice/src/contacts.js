58a08c38be714776ca0f219ae495ccf40525d281
Pulkit Sharma <pulkits94125@gmail.com>
2023-05-27 13:29:36 +0530

    React props practice

---
+++ React props practice/src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Beyonce",
+    imgURL:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    phone: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    name: "Jack Bauer",
+    imgURL:
+      "https://th.bing.com/th/id/OIP.bQWl45dSgEIjLfyRdDWQRwHaHa?w=201&h=201&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  }
+];
+
+export default contacts;
